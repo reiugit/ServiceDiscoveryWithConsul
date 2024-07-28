ec275ce820b486b3f58dac25e809b51b9c93a850
rei <67372053+reiugit@users.noreply.github.com>
2024-07-28 21:25:24 +0200

    Added endpoint information.

--- SampleService/Program.cs
+++ SampleService/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddServiceDiscovery(o => o.UseConsul());
 
 var app = builder.Build();
 
+//this is the service endpoint that will be called after the discovery of this service
 app.MapGet("/", () =>
 {
     return "This response is from SampleService.";
